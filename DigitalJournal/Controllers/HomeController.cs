e2ea067cfd83c16f0ca9444bc134b7646ba0be43
kanadeiar <kanadeiar@gmail.com>
2022-01-22 09:09:00 +0300

    basic core app

---
+++ DigitalJournal/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿namespace DigitalJournal.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult Error(string id)
+    {
+        return id switch
+        {
+            "404" => View("Error404"),
+            _ => Content($"Status --- {id}"),
+        };
+    }
+}
+
--- DigitalJournal/Program.cs
+++ DigitalJournal/Program.cs
@@ -1,6 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureServices(services =>
+{
+    services.AddControllersWithViews().AddRazorRuntimeCompilation();
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStatusCodePagesWithRedirects("~/home/error/{0}");
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
