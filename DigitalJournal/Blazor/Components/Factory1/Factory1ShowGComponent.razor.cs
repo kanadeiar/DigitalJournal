59f22f5cfa4665a0cb10c5633ea2e4918fe701eb
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2022-02-09 08:00:56 +0400

    Отображение данных по смене +

---
+++ DigitalJournal/Blazor/Components/Factory1/Factory1ShowGComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace DigitalJournal.Blazor.Components.Factory1;
+
+public partial class Factory1ShowGComponent
+{
+    [Parameter]
+    public IQueryable<Factory1GeneralShiftData>? Query { get; set; }
+    public Factory1GeneralShiftData? Data { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Data = await Query
+            .Include(x => x.Factory1ProductType)
+            .Include(x => x.Factory1PackProductType)
+            .OrderByDescending(x => x.Time).FirstOrDefaultAsync();
+    }
+}
+
